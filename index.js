7407607b585ca9a8826e6776b74218097388c4b4
imt-eiaz <imteiaz@gmail.com>
2025-06-14 15:04:34 +0100

    button animation done

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ for (i = 0; i < numberOfDrumButtons; i++) {
   document.querySelectorAll(".drum")[i].addEventListener("click", function () {
     var buttonInnerHTML = this.innerHTML;
     makeSound(buttonInnerHTML);
+    buttonAnimation(buttonInnerHTML);
   });
 }
 
@@ -13,6 +14,8 @@ for (i = 0; i < numberOfDrumButtons; i++) {
 
 document.addEventListener("keydown", function (event) {
   makeSound(event.key);
+
+  buttonAnimation(event.key);
 });
 
 function makeSound(key) {
@@ -57,4 +60,12 @@ function makeSound(key) {
 
       break;
   }
+}
+function buttonAnimation(currentKey) {
+  var activeButton = document.querySelector("." + currentKey);
+  activeButton.classList.add("pressed");
+
+  setTimeout(function () {
+    activeButton.classList.remove("pressed");
+  }, "100");
 }
